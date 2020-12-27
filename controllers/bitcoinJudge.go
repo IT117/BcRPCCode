becf717c3cc2684369f443b70e3a3972168e8ac1
IT11717 <2714257837@qq.com>
2020-12-27 17:52:27 +0800

    html

--- controllers/bitcoinJudge.go
+++ controllers/bitcoinJudge.go
@@ -1,6 +1,9 @@
 package controllers
 
-import "BeegoBcRPCCode/utils"
+import (
+	"BeegoBcRPCCode/utils"
+	"fmt"
+)
 
 func BictoinJudge(bitcoin string, target interface{})interface{} {
 	if bitcoin == "getbestblockhash" {
@@ -14,7 +17,17 @@ func BictoinJudge(bitcoin string, target interface{})interface{} {
 	}
 	if bitcoin == "getblock" {
 		Info := utils.GetBlockInfoByHash(target.(string))
-		return Info
+       InfoMap := make(map[string]interface{})
+       InfoMap["该区块的哈希："]=Info.Hash
+       InfoMap["该区块的大小："]=Info.Size
+       InfoMap["该区块的版本："]=Info.Version
+       InfoMap["该区块的高度："]=Info.Height
+
+		fmt.Println("区块的内容：",InfoMap)
+       for k,v :=range InfoMap{
+       	fmt.Println(k,v)
+	   }
+		return InfoMap
 
 	}
 	return nil
